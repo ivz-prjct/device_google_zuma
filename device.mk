8a84c8268f04e81203c9c4e3972b6fbc31fbf729
Ionut <ghermanionut96@gmail.com>
2025-10-14 21:53:54 +0300

    zuma: inherit if available BCR Call Recording
    Signed-off-by: Ionut <ghermanionut96@gmail.com>
    Signed-off-by: Iverz <asterixiverz@gmail.com>

--- device.mk
+++ device.mk
@@ -671,6 +671,9 @@ DEVICE_PRODUCT_COMPATIBILITY_MATRIX_FILE += \
 # AudioFX
 TARGET_EXCLUDES_AUDIOFX := true
 
+# BCR Call Recording
+$(call inherit-product-if-exists, vendor/bcr/bcr.mk)
+
 # DeviceAsWebcam
 TARGET_BUILD_DEVICE_AS_WEBCAM := true
 
