googlepixel8seriesdev/android_device_google_zuma

zuma: add Pixel Camera if exists
Signed-off-by: Ionut Gherman <ghermanionut96@gmail.com>

lineage_common.mk
@@ -16,6 +16,9 @@ PRODUCT_PRODUCT_PROPERTIES += \
     ro.vendor.camera.extensions.package=com.google.android.apps.camera.services \
     ro.vendor.camera.extensions.service=com.google.android.apps.camera.services.extensions.service.PixelExtensions
 
+# Pixel Camera
+$(call inherit-product-if-exists, vendor/google/camera/pixelcamera.mk)
+
 # Face Unlock
 $(call inherit-product-if-exists, vendor/google/faceunlock/device.mk)
 

ivz-prjct/device_google_zuma

lineage_common.mk
@@ -29,6 +29,9 @@ include device/google/gs-common/performance/experiments/experiments.mk
 # Google Assistant
 PRODUCT_PRODUCT_PROPERTIES += ro.opa.eligible_device=true
 
+# Pixel Camera
+$(call inherit-product-if-exists, vendor/google/camera/config.mk)
+
 # Lineage Health
 include hardware/google/pixel/lineage_health/device.mk
 